4283b19b0f02986299359e5ae98542a528a1c82a
Satria Winarah <sri.winarah@gojek.com>
2022-10-05 14:33:27 +0700

    Initial push

---
+++ app/src/main/java/win/satria/lifebar/AddBarActivity.java
@@ -0,0 +1,30 @@
+package win.satria.lifebar;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import win.satria.lifebar.databinding.ActivityAddBarBinding;
+import win.satria.lifebar.databinding.ActivityMainBinding;
+
+public class AddBarActivity extends AppCompatActivity {
+
+    private ActivityAddBarBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAddBarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+    }
+}
